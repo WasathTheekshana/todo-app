e995eed9502acc64f1a6b9880df2797e6eac6fbe
WasathTheekshana <wasath.vt@gmail.com>
2023-01-29 22:26:21 +0530

    dev: create basic background colors and component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+
+const style = {
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-cyan-500 to-blue-500'
+}
 
 function App() {
+
+  const [todos, setTodos] = useState(['Learn React'])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.bg}>
+
+      <div className={style.container}>
+        <h3 className={style.heading}>Todo App</h3>
+        <form className={style.form}>
+          <input className={style.input} type="text" placeholder="Add To-Dooooo" />
+          <button className={style.button}> <AddCircleOutlineIcon /> </button>
+        </form>
+        <ul>
+
+        </ul>
+      </div>
+
     </div>
   );
 }
