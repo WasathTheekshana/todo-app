290ff89075bb9f6639e4b40ead8d42631af9ef07
WasathTheekshana <wasath.vt@gmail.com>
2023-02-02 22:36:21 +0530

    dev: changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
+import Todo from "./Todo";
+
+
 const style = {
   bg: 'h-screen w-screen p-4 bg-gradient-to-r from-cyan-500 to-blue-500'
 }
 
 function App() {
 
-  const [todos, setTodos] = useState(['Learn React'])
+  const [todos, setTodos] = useState(['Learn React','Go to Uni', 'Go to passport offiece'])
 
   return (
     <div className={style.bg}>
@@ -19,7 +22,9 @@ function App() {
           <button className={style.button}> <AddCircleOutlineIcon /> </button>
         </form>
         <ul>
-
+          {todos.map((todo, index) => (
+            <Todo key={index} todo={todo} />
+          ))}
         </ul>
       </div>
 
