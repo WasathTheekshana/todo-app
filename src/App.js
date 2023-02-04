990ed5ef03f9bdcd148395ab87ebc10b589f1892
WasathTheekshana <wasath.vt@gmail.com>
2023-02-04 21:14:28 +0530

    dev: develop

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 
 import Todo from "./Todo";
+import { collection, onSnapshot, query, QuerySnapshot } from "firebase/firestore";
+import { db } from "./firebase";
 
 
 const style = {
-  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-cyan-500 to-blue-500'
+  bg: 'h-screen w-screen p-4 bg-gradient-to-r from-cyan-500 to-blue-500',
+  container: 'bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4',
+  heading: 'text-3xl font-bold text-center text-gray-800 p-2',
+  form: 'flex justify-between',
+  input: 'border p-2 w-full text-xl',
+  button: 'p-4 bg-purple-600 text-slate-100 hover:bg-purple-400 ml-2',
+  count: 'text-center p-2 font-bold',
 }
 
 function App() {
 
-  const [todos, setTodos] = useState(['Learn React','Go to Uni', 'Go to passport offiece'])
+  const [todos, setTodos] = useState([])
+
+  //create todo
+  //Read todo form firebase database
+  useEffect(()=>{
+    const q = query(collection(db, 'todos'))
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArr = []
+      QuerySnapshot.forEach((doc) => {
+        todosArr.push({...doc.data(), id: doc.id})
+      });
+      setTodos(todosArr)
+    });
+    return () => unsubscribe()
+  },[])
+
+  //Update todo in firebaes
+  //delete todo
 
   return (
     <div className={style.bg}>
@@ -26,6 +51,7 @@ function App() {
             <Todo key={index} todo={todo} />
           ))}
         </ul>
+        <p className={style.count}>There are 3 items</p>
       </div>
 
     </div>
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,16 +1,25 @@
 import React from "react";
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 const style = {
-  todo: ""
+  li: 'flex justify-between bg-slate-200 p-4 my-2 capitalize',
+  liComplete: 'flex justify-between bg-slate-400 p-4 my-2 capitalize',
+  row: 'flex',
+  text: 'mx-2 cursor-pointer',
+  textCompelete: 'mx-2 cursor-pointer line-through',
+  button: 'cursor-pointer flex items-center',
 }
 
 const Todo = ({todo}) => {
   return (
-    <li className={style.todo}> 
+    <li className={style.li}> 
       <div className={style.row}>
         <input type="checkbox" />
-        <p className={style.text}>{todo}</p>
+        <p className={style.text}>{todo.text}</p>
       </div>
+      <button>
+        <DeleteOutlineIcon />
+      </button>
     </li>
   )
 }
